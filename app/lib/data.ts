035f60cb1c693d79c8a7c32eb9f22137768397c3
Muhammad Abdullah <imuhammadabdullah@outlook.com>
2024-08-29 03:36:10 +0500

    chapter 12 till form create invoice

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,6 +41,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    console.log("is t doing?");
+    console.log(latestInvoices);
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
